2b97644d4a4ec227f0e8337e05544dbb30255aa1
liuy661 <1053832844@qq.com>
2026-01-06 16:36:37 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,91 +1,27 @@
-import json
-import logging
+@csrf_exempt
+def upload_analyze(request):
+    # 逻辑：不管是文字还是图片，最终都变成一段 Prompt 给 Gemini
+    user_input = ""
+    img_data = None
 
-from django.http import JsonResponse
-from django.shortcuts import render
-from wxcloudrun.models import Counters
+    if request.method == 'POST':
+        # 1. 检查是不是文字输入
+        if request.content_type == 'application/json':
+            data = json.loads(request.body)
+            user_input = data.get('text', '')
+        
+        # 2. 检查是不是图片上传
+        if request.FILES.get('file'):
+            img_file = request.FILES['file']
+            img_data = img_file.read()
+            user_input = "分析这张图里的食物"
 
-
-logger = logging.getLogger('log')
-
-
-def index(request, _):
-    """
-    获取主页
-
-     `` request `` 请求对象
-    """
-
-    return render(request, 'index.html')
-
-
-def counter(request, _):
-    """
-    获取当前计数
-
-     `` request `` 请求对象
-    """
-
-    rsp = JsonResponse({'code': 0, 'errorMsg': ''}, json_dumps_params={'ensure_ascii': False})
-    if request.method == 'GET' or request.method == 'get':
-        rsp = get_count()
-    elif request.method == 'POST' or request.method == 'post':
-        rsp = update_count(request)
-    else:
-        rsp = JsonResponse({'code': -1, 'errorMsg': '请求方式错误'},
-                            json_dumps_params={'ensure_ascii': False})
-    logger.info('response result: {}'.format(rsp.content.decode('utf-8')))
-    return rsp
-
-
-def get_count():
-    """
-    获取当前计数
-    """
-
-    try:
-        data = Counters.objects.get(id=1)
-    except Counters.DoesNotExist:
-        return JsonResponse({'code': 0, 'data': 0},
-                    json_dumps_params={'ensure_ascii': False})
-    return JsonResponse({'code': 0, 'data': data.count},
-                        json_dumps_params={'ensure_ascii': False})
-
-
-def update_count(request):
-    """
-    更新计数，自增或者清零
-
-    `` request `` 请求对象
-    """
-
-    logger.info('update_count req: {}'.format(request.body))
-
-    body_unicode = request.body.decode('utf-8')
-    body = json.loads(body_unicode)
-
-    if 'action' not in body:
-        return JsonResponse({'code': -1, 'errorMsg': '缺少action参数'},
-                            json_dumps_params={'ensure_ascii': False})
-
-    if body['action'] == 'inc':
-        try:
-            data = Counters.objects.get(id=1)
-        except Counters.DoesNotExist:
-            data = Counters()
-        data.id = 1
-        data.count += 1
-        data.save()
-        return JsonResponse({'code': 0, "data": data.count},
-                    json_dumps_params={'ensure_ascii': False})
-    elif body['action'] == 'clear':
-        try:
-            data = Counters.objects.get(id=1)
-            data.delete()
-        except Counters.DoesNotExist:
-            logger.info('record not exist')
-        return JsonResponse({'code': 0, 'data': 0},
-                    json_dumps_params={'ensure_ascii': False})
-    else:
-        return JsonResponse({'code': -1, 'errorMsg': 'action参数错误'},
-                    json_dumps_params={'ensure_ascii': False})
+        # 3. 呼叫 Gemini
+        prompt = f"你是毒舌减肥精算师'斤斤'。用户说/发了：{user_input}。请给出热量估算和吐槽。返回JSON。"
+        
+        if img_data:
+            response = model.generate_content([prompt, {'mime_type': 'image/jpeg', 'data': img_data}])
+        else:
+            response = model.generate_content(prompt)
+            
+        return JsonResponse(json.loads(response.text))
